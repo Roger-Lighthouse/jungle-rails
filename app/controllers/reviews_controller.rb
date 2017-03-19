77db5c11d293e266f00d3abc18dd269089eea124
Roger-Lighthouse <roger@whiteshark.ca>
2017-03-19 17:25:20 -0400

    ratings

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,35 @@
+class ReviewsController < ApplicationController
+
+
+  before_action :require_login
+
+  def create
+    #raise 'Got here'
+    review = Review.new(review_params)
+    review.user_id = current_user.id
+    review.product_id = params[:product_id]
+    review.save!
+    redirect_to products_path(:id => review.product_id)
+  end
+
+  def delete
+    id = params[:id]
+    rev = Review.find(id)
+    rev.destroy!
+    redirect_to products_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating, :user_id, :product_id)
+  end
+
+  def require_login
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to products_url # halts request cycle
+    end
+  end
+
+end
